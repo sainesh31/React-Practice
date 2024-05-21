b2d844e360a2b53e060040bff9e1390bafbfd8df
sainesh31 <saineshkchandani31@gmail.com>
2024-05-22 00:39:09 +0530

    React project

--- 02-JS-review/starter/script.js
+++ 02-JS-review/starter/script.js
@@ -296,6 +296,7 @@ booksAfterUpdate;
 async function getTodos() {
   const res = await fetch("https://jsonplaceholder.typicode.com/todos");
   const data = await res.json();
+  console.log(data);
 
   return data;
 }
